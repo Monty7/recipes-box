a9bdbbb442246ae830faa5591327845946d53ec4
M Banks <montensia1@outlook.com>
2019-06-06 02:26:26 +0000

    First commit

---
+++ recipes-box/lib/recipes/box.rb
@@ -0,0 +1,8 @@
+require "recipes/box/version"
+
+module Recipes
+  module Box
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ recipes-box/lib/recipes/box/version.rb
@@ -0,0 +1,5 @@
+module Recipes
+  module Box
+    VERSION = "0.1.0"
+  end
+end
