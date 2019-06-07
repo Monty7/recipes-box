8cc1ec88f7a3a208d9e665f28a12e4363accd912
M Banks <montensia1@outlook.com>
2019-06-07 04:06:53 +0000

    scraping the titles

--- recipes-box/lib/scraper.rb
+++ recipes-box/lib/scraper.rb
@@ -1,13 +1,20 @@
 class Scraper 
-  
+  attr_accessor :recipe_titles
   def self.get_page 
   #  page = open("https://www.connoisseurusveg.com/recipe-index/")
+  @recipe_titles = {}
     page = open("https://sweetpotatosoul.com/recipes")
     sleep 5
     doc = Nokogiri::HTML(page)
-    elements = doc.css()
-   # elements = doc.css(".entry-header .entry-title a")
-    binding.pry #how do i use pry without 'learn'
+    titles = doc.css('.recipe h4 a')
+      
+    titles.each do |title|
+      binding.pry
+      
+     # :recipe_titles[title] = {:url = title.attr('href')}
+      
+    end
+    #binding.pry
   end
   
 end
