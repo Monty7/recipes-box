60297a5281419b6d7bc1c0660ce9dd18e6cf847a
M Banks <montensia1@outlook.com>
2019-06-10 17:38:36 +0000

    added recipe class for recipe instances

--- recipes-box/lib/recipes.rb
+++ recipes-box/lib/recipes.rb
@@ -1,43 +1,14 @@
-module Recipes
-  module ClassMethods
-   def get_recipe_titles
-      counter = 0
-      recipe_list = []
-      self.get_page.each do |key, value|
-        counter += 1
-        recipe_list << [counter, value[:title]]
-    
-        puts "#{counter}. #{value[:title]}"
-      end
-      recipe_list
-     # binding.pry
-    end
+class Recipe
+  attr_accessor :title, :url
   
-    # def self.add_recipe
-    #   select_title
-    # end
-    
-     def select_title(num)
-       selected_title = ""
-       get_recipe_titles.each do |title|
-        
-        if title[0] == num.to_i
-          puts title
-          puts "Do you want to add this recipe to your recipe box list? 'Y' or 'N' "
-          input = gets.chomp.downcase 
-          case input
-            when 'y'
-             
-              selected_title = title[1]
-              @box << selected_title
-              
-              #binding.pry
-              #all_user_recipes(title)
-             # CLI.add_recipe(selected_title)
-            end
-       end
-      end
-      selected_title
-     end
+  @@all = []
+  
+  def initialize(title, url)
+    @title = title
+    @url = url
+  end
+  
+  def all
+    @all
   end
 end
--- recipes-box/lib/scraper.rb
+++ recipes-box/lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
   
     titles.each do |title|
      
-      title_txt = title.text.to_sym
+      title_txt = title.text
       url = title.attr('href')
   
       recipes[title_txt] = {:title => title_txt, :url => url}
@@ -24,45 +24,45 @@ class Scraper
   
 
 
-  # def self.get_recipe_titles
-  #   counter = 0
-  #   recipe_list = []
-  #   self.get_page.each do |key, value|
-  #     counter += 1
-  #     recipe_list << [counter, value[:title]]
+  def self.get_recipe_titles
+    counter = 0
+    recipe_list = []
+    self.get_page.each do |key, value|
+      counter += 1
+      recipe_list << [counter, value[:title]]
   
-  #     puts "#{counter}. #{value[:title]}"
-  #   end
-  #   recipe_list
-  # # binding.pry
-  # end
+      puts "#{counter}. #{value[:title]}"
+    end
+    recipe_list
+  # binding.pry
+  end
 
-  # # def self.add_recipe
-  # #   select_title
-  # # end
-  
-  # def self.select_title(num)
-  #   selected_title = ""
-  #   get_recipe_titles.each do |title|
-      
-  #     if title[0] == num.to_i
-  #       puts title
-  #       puts "Do you want to add this recipe to your recipe box list? 'Y' or 'N' "
-  #       input = gets.chomp.downcase 
-  #       case input
-  #         when 'y'
-  #           binding.pry
-  #           selected_title = title[1]
-  #           #@box << selected_title
-            
-  #           #binding.pry
-  #           #all_user_recipes(title)
-  #         # CLI.add_recipe(selected_title)
-  #         end
-  #   end
-  #   end
-  #   selected_title
+  # def self.add_recipe
+  #   select_title
   # end
+  
+  def self.select_title(num)
+    selected_title = ""
+    get_recipe_titles.each do |title|
+      
+      if title[0] == num.to_i
+        puts title
+        puts "Do you want to add this recipe to your recipe box list? 'Y' or 'N' "
+        input = gets.chomp.downcase 
+        case input
+          when 'y'
+            binding.pry
+            selected_title = title[1]
+            #@box << selected_title
+            
+            #binding.pry
+            #all_user_recipes(title)
+          # CLI.add_recipe(selected_title)
+          end
+    end
+    end
+    selected_title
+  end
    
  
 end
